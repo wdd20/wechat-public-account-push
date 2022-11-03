98640a84b4b89d360e1abba828a9784f00c04a0a
wdd20 <117266055+wdd20@users.noreply.github.com>
2022-11-03 11:12:24 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '悦悦',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ozFaa5nUEW8ZOoidGy931-hJubyI',
+      id: 'ozFaa5os-JDYdARUmCsgX8vGw2gw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'fJutHF1nnR3yydN93iREYaMlezTFu7mVQHMEl7Ugv5E',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
